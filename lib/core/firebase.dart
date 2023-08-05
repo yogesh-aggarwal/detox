a7a1aa7e6a5590bf0bae3fee2ebdf01890e0c992
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2023-08-05 20:35:28 +0530

    Made basic firebase setup and enabled dynamic theming

---
+++ lib/core/firebase.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> initializeFirebase() async {
+  await Firebase.initializeApp();
+}
+
+final auth = FirebaseAuth.instance;
+final firestore = FirebaseFirestore.instance;
+
+final usersColl = firestore.collection('Users');
+final timeTracksColl = firestore.collection('Time Tracks');
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:detox/core/firebase_options.dart';
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MultiProvider(
       providers: [
@@ -18,6 +24,71 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        return MaterialApp(
+          title: "Detox",
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: lightDynamic,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkDynamic,
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.system,
+          home: Detox(),
+        );
+      },
+    );
+  }
+}
+
+class Detox extends StatefulWidget {
+  const Detox({super.key});
+
+  @override
+  State<Detox> createState() => _DetoxState();
+}
+
+class _DetoxState extends State<Detox> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detox"),
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: const <NavigationDestination>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.timer),
+            icon: Icon(Icons.timer_outlined),
+            label: 'Tracker',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.analytics),
+            icon: Icon(Icons.analytics_outlined),
+            label: 'Analysis',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }
